f3adfd8827d75e1c141aef68f1feef154c608e78
redisnotbluedev <ededmaude@gmail.com>
2025-11-09 11:42:02 +1030

    add image

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def index():
 	response.headers["Link"] = '</static/style.css>; rel="stylesheet"'
 	return response
 
+@app.route("/button_text.svg")
+def button_text():
+	text = "On"
+	response = Response(f"""<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+  <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" font-size="24">
+    {text}
+  </text>
+</svg>""", mimetype="image/svg+xml")
+	return response
+
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
