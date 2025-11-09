ea489a36957726c16bed9dc3deb788731606134b
redisnotbluedev <ededmaude@gmail.com>
2025-11-09 11:53:09 +1030

    without the viewbox?

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ def index():
 @app.route("/button_text.svg")
 def button_text():
 	text = "On"
-	response = Response(f"""<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" preserveAspectRatio="none">
+	response = Response(f"""<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
 	<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" font-size="24">
 		{text}
-  	</text>
+	</text>
 </svg>""", mimetype="image/svg+xml")
 	return response
 
